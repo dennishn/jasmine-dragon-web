2d709225792637a1672ae68be20df60526ee0c01
Dennis Haulund Nielsen <Dennis.Nielsen@akqa.com>
2026-07-28 10:00:45 +0200

    set up tooling

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    poweredByHeader: false,
+    reactCompiler: true,
+    reactStrictMode: true,
+    typedRoutes: true,
+    cacheComponents: true,
+    partialPrefetching: true,
+    experimental: {
+        useTypeScriptCli: true,
+        typedEnv: true,
+        inlineCss: true,
+        useCache: true,
+        viewTransition: true,
+        webVitalsAttribution: ["FCP", "LCP", "CLS", "TTFB", "INP"],
+    },
+    logging: {
+        browserToTerminal: true,
+    },
 };
 
 export default nextConfig;
