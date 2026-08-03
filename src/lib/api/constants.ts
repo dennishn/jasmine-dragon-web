0456c509cc3e600467e7afb5991ebdd08c4690c0
Dennis Haulund Nielsen <Dennis.Nielsen@akqa.com>
2026-08-03 12:31:54 +0200

    update constants

--- src/lib/api/constants.ts
+++ src/lib/api/constants.ts
@@ -16,4 +16,49 @@ const LOOT_MODE = {
     LOOT_COUNCIL: "LOOT_COUNCIL",
 } as const;
 
-export { API_BASE_URL, VERIFICATION_STATUS, TEAM_ID, LOOT_MODE };
+const TEAMS = {
+    TEAM_JASMINE: "team-jasmine",
+    TEAM_DRAGON: "team-dragon",
+};
+
+const AUTHENTICATION_PROVIDER = {
+    DISCORD: "DISCORD",
+    BOOTSTRAP: "BOOTSTRAP",
+} as const;
+
+const ELIGIBLE_ROLE = {
+    TANK: "TANK",
+    HEALER: "HEALER",
+    DPS: "DPS",
+} as const;
+
+const RAID_STATUS = {
+    DRAFT: "DRAFT",
+    PUBLISHED: "PUBLISHED",
+    CANCELLED: "CANCELLED",
+    COMPLETED: "COMPLETED",
+} as const;
+
+const SIGNUP_STATUS = {
+    ACCEPTED: "ACCEPTED",
+    TENTATIVE: "TENTATIVE",
+    DECLINED: "DECLINED",
+} as const;
+
+const LOOT_AWARD_STATUS = {
+    ACTIVE: "ACTIVE",
+    VOIDED: "VOIDED",
+} as const;
+
+export {
+    API_BASE_URL,
+    VERIFICATION_STATUS,
+    TEAM_ID,
+    LOOT_MODE,
+    TEAMS,
+    AUTHENTICATION_PROVIDER,
+    ELIGIBLE_ROLE,
+    RAID_STATUS,
+    SIGNUP_STATUS,
+    LOOT_AWARD_STATUS,
+};
